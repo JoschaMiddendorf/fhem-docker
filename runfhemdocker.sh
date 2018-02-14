2f77cef0729d5221b9345d17f53ae25b8269bfe0
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-02-14 17:08:36 +0100

    Update runfhemdocker.sh

--- runfhemdocker.sh
+++ runfhemdocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run  --name fhem2 --cap-add SYS_ADMIN \
+docker run  --name fhem --cap-add SYS_ADMIN \
    -p 7072:7072 -p 8083:8083 -p 8084:8084 -p 8085:8085 -p 2222:2222 -p 9001:9001 \
    -v /var/fhemdocker/fhem:/opt/fhem  \
 fhem_jessie
