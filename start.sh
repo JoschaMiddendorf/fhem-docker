df053c6426c340941e62c6439c1d85aa837f55f8
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-02-21 18:21:34 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ set -e
 port=7072
 
 echo "Extract FHEM config data to /opt/fhem/ if empty:"
-echo "-->"
+echo ""
 /root/_cfg/volumedata2.sh write /opt/fhem 
+echo ""
 echo ""
 
 echo "Starting FHEM:"
-echo "-->"
-perl /opt/fhem/fhem.pl /opt/fhem/fhem.cfg
-tee /opt/fhem/log/fhem.log
+echo ""
+perl /opt/fhem/fhem.pl /opt/fhem/fhem.cfg | tee /opt/fhem/log/fhem.log
