5fffb1b40688162dae3d1b5da5815e7115fd6e8d
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-02-22 03:16:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,10 @@ echo ""
 /root/_cfg/volumedata2.sh write /opt/fhem 
 chown -R root:root /opt/fhem
 echo ""
-echo ""
+#echo "Extract FHEM config data to /opt/fhem/ if empty:"
+#echo -en "set ${1} ${2}\nquit\n" | nc -w 5 fhem.sever 7072
+#initial update according to https://forum.fhem.de/index.php?topic=44921.0
+#echo ""
 
 echo "Starting FHEM:"
 echo ""
