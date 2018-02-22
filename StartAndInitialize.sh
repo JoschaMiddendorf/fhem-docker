a58956848f2b720f8bf2a85f362e4ef051ad630b
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-02-22 14:19:53 +0100

    Update StartAndInitialize.sh

--- StartAndInitialize.sh
+++ StartAndInitialize.sh
@@ -1,93 +1,92 @@
 #!/bin/bash
+##################################################################################################
+##
+##	Start and intitialization script for fhem-docker
+##	Copyright (c) 2018 Joscha Middendorf
+##
+##	Before mounting a volume to the container, this script comresses the content 
+##	of a provided configuration directory to /root/_config.
+##	usage:	./StartAndInitialize.sh Arg1=initialize Arg2=/abs/path/to/directory/
+##
+##	After mounting a volume to the container, this script extracts the content 
+##	of the previously compressed configuration directory back to a provided directory,
+##	if the directory is empty.
+##	usage:	./StartAndInitialize.sh Arg1=extract Arg2=/abs/path/to/directory/
+##
+##################################################################################################
 
-set -e
-port=7072
+### Functions to start FHEM ###
 
-echo ""
-echo "-------------------------------------------------------------------------------------------------------------------------"
-echo "Extract FHEM config data to /opt/fhem/ if empty:"
-echo ""
-/root/_cfg/volumedata2.sh write /opt/fhem 
-chown -R root:root /opt/fhem
-echo ""
-#echo "Extract FHEM config data to /opt/fhem/ if empty:"
-#echo -en "set ${1} ${2}\nquit\n" | nc -w 5 fhem.sever 7072
-#initial update according to https://forum.fhem.de/index.php?topic=44921.0
-#echo ""
+function StartFHEM {
+	echo ''
+	echo 'Starting FHEM:'
+	echo ''
+	cd /opt/fhem
+	perl fhem.pl fhem.cfg | tee /opt/fhem/log/fhem.log
+}
+function StartFHEMandUpdate {
+	echo ''
+	echo '!  Almost ready... You are about to start FHEM for the first time.'
+	echo '!  Pleast connect to FHEM via http://YourLocalIP:8083 '
+	echo '!  and execute the command "update" (without the "") first before you do anything else."'
+	echo '!  As soon as the update is complete, execute "shutdown restart" and have fun!'
+	echo ''
+	echo Starting FHEM:
+	echo ''
+	cd /opt/fhem
+	perl fhem.pl fhem.cfg | tee /opt/fhem/log/fhem.log
+}
 
-echo "Starting FHEM:"
-echo ""
-cd /opt/fhem
-perl fhem.pl fhem.cfg | tee /opt/fhem/log/fhem.log 
-#&& tail -f /opt/fhem/log/fhem.log
+### Start of Script ###
 
-
-
-
-#!/bin/bash
-##############################################################################
-# cfg script for volumedata
-# (c) armin@pipp.at 01/2017
-#
-# This tool extracts files from the initial config process from a tar file.
-# If files exist, no one are overwritten!
-#
-# usage:   ./volumedata2.sh <mode <dir
-# EXAMPLE: ./volumedata2.sh write /var/www
-#           modes: create = makes tgz file
-#                   write = extract from tgz
-###########################################################################
-
+echo ''
+echo '-------------------------------------------------------------------------------------------------------------------------'
+echo 'Extract FHEM config data to /opt/fhem/ if empty:'
+echo ''
 
 if [ -z $2 ]; then
-    echo "Error: Missing mode or dir as parameter! IE: create /var/www " 
-    exit
+    echo 'Error: Not enough arguments provided, please provide Arg1=initialize/extract and Arg2=/abs/path/to/directory/'
+    exit 1
 fi
 
-# echo "The script you are running has basename `basename $0`, dirname `dirname $0`"
-
-DIR=`dirname $0`
-TGZDIR=/_cfg
-test -e $TGZDIR || mkdir -p $TGZDIR 
-
-# echo CFGdir=$DIR
-# echo TGZdir=$TGZDIR
+PACKAGEDIR=/root/_config
+test -e $PACKAGEDIR || mkdir -p $PACKAGEDIR 
 
 case $1 in
-	create)
-		# echo create $2
-		# if  dir exist
+	initialize)
+		## check if $2 is a extsting directory
 		if  [ -d  $2 ]; then  
-			TGZ=$TGZDIR/`echo $2 | sed -r 's/\//_/g'`.tgz
-			echo create $TGZ from $2
-			tar -czf $TGZ $2
+			PACKAGE=$PACKAGEDIR/`echo $2 | tr '[/]' '-'`.tgz
+			tar -fcz $PACKAGE $2018
+			echo "Created package $PACKAGE from $2."
 		fi
 		;;
-	write)
-		# echo write $2
-		TGZ=$TGZDIR/`echo $2 | sed -r 's/\//_/g'`.tgz
-		
-		if [ -e $TGZ.firstrun ]; then
-			 echo firstrun $2 already done, exit
+	extract)
+		## check if $PACKAGE was extracted before
+		PACKAGE=$PACKAGEDIR/`echo $2 | tr '[/]' '-'`.tgz
+		if [ -e $PACKAGE.extracted ]; then
+			echo "The package $PACKAGE was already extracted before, no extraction processed!"
+			StartFHEM
 			exit
 		fi
 		
-		# extract only if dir not empty
+		# check if directory $2 is empty
 		if 	[ "$(ls -A $2)" ]; then
-			echo "$2 not empty - no extraction, exit!"
+			echo "Directory $2 isn't empty, no extraction processed!"
+			StartFHEM
 			exit
-			else 
-			if [ -e $TGZ ]; then
-				# -C /destdirroot  -k, --keep-old-files
-				# -k do not overwrite existsing files
-				echo extract $TGZ to $2 
-				tar -xzkf $TGZ -C / 
-				touch $TGZ.firstrun
+		else 
+			# check if $PACKAGE exists
+			if [ -e $PACKAGE ]; then
+				tar -xzkf $PACKAGE -C / 
+				touch $PACKAGE.extracted
+				echo "Extracted package $PACKAGE to $2"
+				StartFHEMandUpdate
 			fi
 		fi	
 		;;
 	*)
-	echo wrong parameter
-	exit 1
+		echo 'Error: Wrong arguments provided, please provide Arg1=initialize/extract and Arg2=/abs/path/to/directory/'
+		exit 1
 	;;
 esac
