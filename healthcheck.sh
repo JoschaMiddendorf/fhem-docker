84b69dbfbd8a7950de10cf1808c02dddb47bfee0
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-02-27 15:29:12 +0100

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,8 +2,8 @@
 
 CONFIG_FILE=/opt/fhem/fhem.cfg
 if [ -f "${CONFIG_FILE}" ]; then
-    FHEM_PORT=$(grep "^FHEMWEB" ${CONFIG_FILE} | cut -d' ' -f4)
+        FHEM_PORT=$(grep -m 1 "define .* FHEMWEB .* global" ${CONFIG_FILE} | awk '{print $4}')
 fi
 PORT=${FHEM_PORT:-8083}
 
-curl -kILs --fail http://localhost:"${PORT}"
+curl -ks --fail http://localhost:"${PORT}" && exit 0 || exit 1
