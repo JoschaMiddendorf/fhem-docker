2997af4d110eae0abcf887077d614f0b3a41c3ef
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-02-28 09:29:57 +0100

    Update StartAndInitialize.sh

--- StartAndInitialize.sh
+++ StartAndInitialize.sh
@@ -40,7 +40,7 @@ function StartFHEM {
 		perl fhem.pl 7072 shutdown
 		echo 'Waiting for FHEM process to terminate before stopping container:'
 		echo
-		grep -q "Server shutdown" <(tail -f "$(date +"$LOGFILE")")					## Wait for FHEM to stop
+		grep -q "Server shutdown" <(tail -f -n0 "$(date +"$LOGFILE")")					## Wait for FHEM to stop
 		PrintNewLines
 		echo
 		echo 'FHEM process terminated, stopping container. Bye!'
@@ -54,7 +54,7 @@ function StartFHEM {
 	cd /opt/fhem || exit 1
 	trap "StopFHEM" SIGTERM
 	perl fhem.pl fhem.cfg
-	grep -q "Server started" <(tail -f "$(date +"$LOGFILE")")						## Wait for FHEM to start up
+	grep -q "Server started" <(tail -f -n0 "$(date +"$LOGFILE")")						## Wait for FHEM to start up
 	PrintNewLines
 	
 	## Evetually update FHEM
@@ -63,13 +63,13 @@ function StartFHEM {
 		echo 'Performing initial update of FHEM, this may take a minute...'
 		echo
 		perl /opt/fhem/fhem.pl 7072 update > /dev/null
-		grep -q "update finished" <(tail -f "$(date +"$LOGFILE")")					## Wait for update to finish
+		grep -q "update finished" <(tail -f -n0 "$(date +"$LOGFILE")")					## Wait for update to finish
 		PrintNewLines
 		echo
 		echo 'Restarting FHEM after initial update...'
 		echo
 		perl /opt/fhem/fhem.pl 7072 "shutdown restart"
-		grep -q "Server started" <(tail -f "$(date +"$LOGFILE")")					## Wait for FHEM to start up
+		grep -q "Server started" <(tail -f -n0 "$(date +"$LOGFILE")")					## Wait for FHEM to start up
 		PrintNewLines
 		echo
 		echo 'FHEM updated and restarted!'
