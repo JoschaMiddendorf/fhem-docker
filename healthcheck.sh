fd8eb875be71920847fe5afb56402e0fcc3a5de0
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-03-08 13:54:41 +0100

    Update healthcheck.sh
    Added option to pass basic auth credentials in form of user:password to healthchech script by variable HEALTHCHECKCREDENTIALS

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 ##################################################################################################
 ##
-##	Healthcheck script for fhem-docker
-##	Copyright (c) 2018 Joscha Middendorf
+##      Healthcheck script for fhem-docker
+##      Copyright (c) 2018 Joscha Middendorf
 ##
-##	This script scanns fhem.cfg for the first FHEMWEB device and determines
-##      it's PORT and NAME, afterwards it determines if the HTTPS attribute for the 
+##      This script scanns fhem.cfg for the first FHEMWEB device and determines
+##      it's PORT and NAME, afterwards it determines if the HTTPS attribute for the
 ##      device is set to 1.
 ##
-##      Now that it knows on witch PORT FHEM is running and if FHEM runs on HTTP or HTTPS 
-##      it starts a corisponding curl request via localhost and reports the reachability 
+##      Now that it knows on witch PORT FHEM is running and if FHEM runs on HTTP or HTTPS
+##      it starts a corisponding curl request via localhost and reports the reachability
 ##      via exit code 0/1 to set docker health status to healthy/unhealthy.
 ##
 ##################################################################################################
 
 CONFIG_FILE=/opt/fhem/fhem.cfg
 if [ -f "${CONFIG_FILE}" ]; then
-        FHEMWEB_PORT=$(^grep -m 1 "define .* FHEMWEB .* global$" ${CONFIG_FILE} | awk '{print $4}')
-        FHEMWEB_NAME=$(^grep -m 1 "define .* FHEMWEB .* global$" ${CONFIG_FILE} | awk '{print $2}')
+        FHEMWEB_PORT=$(grep -m 1 "define .* FHEMWEB .* global$" ${CONFIG_FILE} | awk '{print $4}')
+        FHEMWEB_NAME=$(grep -m 1 "define .* FHEMWEB .* global$" ${CONFIG_FILE} | awk '{print $2}')
 fi
 
 grep -q "^attr $FHEMWEB_NAME HTTPS 1$" ${CONFIG_FILE} && HTTP_S='https' || HTTP_S='http'
+
 PORT=${FHEMWEB_PORT:-8083}
 
-curl -ks --fail "$HTTP_S"://localhost:"${PORT}" && exit 0 || exit 1
+test -z "$HEALTHCHECKCREDENTIALS" && CREDENTIALS="" || CREDENTIALS="--user $HEALTHCHECKCREDENTIALS"
+
+curl -ks $CREDENTIALS --fail "$HTTP_S"://localhost:"${PORT}" && exit 0 || exit 1
